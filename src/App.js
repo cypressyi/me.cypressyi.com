bf97085e8b229083908f109943ad7de0b3330502
Cypressyi <cypressyi1990@gmail.com>
2017-12-25 11:39:56 +0800

    Add ESLint and airbnb js style.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Button } from 'reactbulma';
 import styled from 'styled-components';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 
 const StyledButton = styled(Button)`
@@ -12,23 +12,22 @@ margin: 1em;
 padding: 0.25em 1em;
 border: 2px solid palevioletred;
 border-radius: 3px;
-`
+`;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <StyledButton primary>Primary</StyledButton>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <StyledButton primary>Primary</StyledButton>
+    </div>
+  );
+};
+
 
 export default App;
