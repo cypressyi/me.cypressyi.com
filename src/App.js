61f395d8d6a949597e6e6d90202c25c96a7b2b67
Cypressyi <cypressyi1990@gmail.com>
2018-02-08 13:50:32 +0800

    Feat: Move track system from google analytics to google tag manager.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import { Grid } from 'semantic-ui-react';
 import React from 'react';
 import { Divider } from 'antd';
-import ReactGA from 'react-ga';
 import './App.css';
 import Profile from './components/Profile';
 import Summary from './components/Summary';
@@ -9,9 +8,6 @@ import Skill from './components/Skill';
 import Experience from './components/Experience';
 import Education from './components/Education';
 import Portfolio from './components/Portfolio';
-
-ReactGA.initialize('UA-93114522-4');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = (props) => {
   const {
